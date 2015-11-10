7c761b7c6ea72b54844644d948f0083e9b971b8f
manoloIESvirgendelcarmen <manu.chinaski@gmail.com>
2015-11-10 20:53:37 +0100

    Fichero Interes.java

---
+++ Interes.java
@@ -0,0 +1,11 @@
+//fichero Interes.java
+//Clase el interés (I) que produce un capital
+//el cúal  es directamente proporcional al capital inicial (C),
+// al tiempo (t), y a la tasa de interés (i) :
+//I = C · i · t
+
+
+public static double calcularInter(argumentos){
+   //código para calcular el interes
+   //return interes
+}
