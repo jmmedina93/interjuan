d5d4ec2fa32cbc5b4dcf7e678f5ab5ee3f60a1a4
manoloIESvirgendelcarmen <manu.chinaski@gmail.com>
2015-11-10 20:54:25 +0100

    Fichero TestInteres.java

---
+++ Testnteres.java
@@ -0,0 +1,15 @@
+//fichero Interes.java
+//leemos desde un Scanner los valores
+//capital, tiempo y tasa de  interés
+//y mediante el  método calcularInteres de la
+//clase  Interes muestra por pantalla el interés
+//calculado
+
+public class Interes{
+   public static void main(String[] arg){
+        //preparamos el scanner
+        //lectura de lo datos
+        //llamada al método calcularInteres
+        //mostramos por pantalla el valor del interés
+   }
+}
